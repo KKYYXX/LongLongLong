25339750bd9852eca4ffe0d64d75c2077f56767f
Jiang <20042978@qq.com>
2025-07-29 18:43:46 +0800

    到条件查询

--- app/views.py
+++ app/views.py
@@ -17,11 +17,43 @@ def getDepInfo(id):
 #记录添加
 @blue.route('/dept/add',methods=['POST'])
 def addDepinfo():
+
     try1 = (TryModel(
-    name = request.args.get('name'),
-    id =request.args.get('id')
+    name = request.form.get('name'),
+    id =request.form.get('id',type=int)
     ))
     db.session.add(try1)
     db.session.commit()
 
     return "添加信息成功"
+
+#记录更新
+@blue.route('/dept/upd',methods=['POST'])
+def updDepinfo():
+
+    try2 = TryModel.query.get(request.form.get('id'))
+    try2.name = request.form.get('name')
+
+    db.session.commit()
+
+    return "修改信息成功"
+
+#记录删除
+@blue.route('/dept/del',methods=['POST'])
+def delDepinfo():
+
+    try3 = TryModel.query.get(request.form.get('id'))
+
+    db.session.delete(try3)
+
+    db.session.commit()
+
+    return "删除信息成功"
+
+#信息查询（条件查询）显示的是列表的形式
+@blue.route('/dept/find',methods=['GET'])
+def finddepts():
+
+    info_1 = TryModel.query.filter(TryModel.name == '王五').all()
+
+    return '查询成功'
