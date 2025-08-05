7e0e3085fd509530d0a97081ccc4a5be60470877
xjy <3311980850@qq.com>
2025-08-05 13:16:09 +0800

    改接口

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from app.plugins import initPlugins
 from app.views import blue
-
-
+from flask_cors import CORS
+  # 允许所有跨域请求
 def createApp():
 
     app = Flask(__name__)
-
+    CORS(app)  # 允许所有跨域请求
     #注册蓝图（蓝图名字，url前缀？)
     app.register_blueprint(blue,url_prefix='/app')
 
